d20234cf1baa05d42e708e1b2f546cc527c12f06
GenixPL <intergenixyt@gmail.com>
2026-09-18 12:37:26 +0200

    start

---
+++ lib/second_screen.dart
@@ -0,0 +1,14 @@
+import 'package:auto_route/annotations.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage(
+  name: '/second',
+)
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
